30b638a998f8100f391694205667c849954ad9db
Syuzanna Eprikyan <syuzanna.eprikyan@gmail.com>
2020-04-18 16:28:34 +0400

    Updates

---
+++ precache-manifest.877644c5b557b671d33fc1a6f31dc10d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e94bc9da684d8814bd0d0a71b597f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b3abec953c80f9d35969",
+    "url": "/monsters/static/css/main.b5006e8b.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3abec953c80f9d35969",
+    "url": "/monsters/static/js/main.56ceda4b.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
